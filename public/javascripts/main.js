5a8addf420ecd43f3de7450e99dd0cc80967b315
Jaeho Cho <real0131@gmail.com>
2016-03-01 14:07:45 +0900

    backup

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,4 @@
-/*
+
 var nav_a = document.getElementById('nav_a');
 var nav_b = document.getElementById('nav_b');
 var nav_c = document.getElementById('nav_c');
@@ -6,6 +6,22 @@ var nav_c = document.getElementById('nav_c');
 var content_1 = document.getElementById('content_1');
 var content_2 = document.getElementById('content_2');
 var content_3 = document.getElementById('content_3');
+function nav_a_onclick(){
+    content_1.style.visibility = "visible";
+    content_2.style.visibility = "hidden";
+    content_3.style.visibility = "hidden";
+}
+function nav_b_onclick(){
+    content_1.style.visibility = "hidden";
+    content_2.style.visibility = "visible";
+    content_3.style.visibility = "hidden";
+}
+function nav_c_onclick(){
+    content_1.style.visibility = "hidden";
+    content_2.style.visibility = "hidden";
+    content_3.style.visibility = "visible";
+}
+/*
 nav_a.onclick = function(){
     if(content_2.style.visibility != "hidden")
     {
